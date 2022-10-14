fa5502a5efce63122324aba038c7b4a263587227
Maxim Melichov <80150303+maxmelichov@users.noreply.github.com>
2022-10-14 12:00:59 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,58 @@
+from geolocation.main import GoogleMaps
+from geolocation.distance_matrix.client import DistanceMatrixApiClient
+import geopy.distance
+from tkinter import *
+
+
+class App(object):
+    def __init__(self, root):
+        self.address1 =Text (root,height=1,width=20,font=("Helvetica", 10))
+        self.address1.place(x=200,y=50)
+        self.address1.pack()
+        self.address2 = Text (root,height=1,width=20,font=("Helvetica", 10))
+        self.address2.place(x=200,y=100)
+        self.address2.pack()
+        self.google_maps = GoogleMaps(api_key="your_google_maps_key")
+        self.button = Button(root, text="Calculate", command=self.calculate_dis)
+        self.button.place(x=170,y=60)
+        
+    def calculate_dis(self):
+        location1 = self.google_maps.search(location= self.address1.get("1.0", "end-1c"),) # sends search to Google Maps.
+        location2 = self.google_maps.search(location= self.address2.get("1.0", "end-1c"),) # sends search to Google Maps.
+        my_location1 = location1.first() # returns only first location.
+        my_location2 = location2.first() # returns only first location.
+        coords_1 = (my_location1.lat, my_location1.lng)
+        coords_2 = (my_location2.lat, my_location2.lng)
+        dis = geopy.distance.geodesic(coords_1, coords_2).km
+        
+        if dis <=15.0:
+            self.label1 =Label(text= "You are in the YELLOW zone 5.5 shekel is your payment ",font=("Helvetica", 12),background= 'yellow')
+        elif dis >15.0 and dis <=40.0:
+            self.label1 =Label(text= "You are in the GREEN zone 12 shekel is your payment ",font=("Helvetica", 12),background= 'green')
+        elif dis >40.0 and dis <=75.0:
+            self.label1 =Label(text= "You are in the LIGHT BLUE zone 16 shekel is your payment ",font=("Helvetica", 12),background= 'light blue')
+        elif dis >75.0 and dis <=120.0:
+            self.label1 =Label(text= "You are in the BLUE zone 16 shekel is your payment ",font=("Helvetica", 12),background= 'blue')
+        elif dis >120.0 and dis <=225.0:
+            self.label1 =Label(text= "You are in the PURPLE zone 27 shekel is your payment ",font=("Helvetica", 12),background= 'purple')
+        elif  dis >225.0:
+            self.label1 =Label(text= "You are in the GRAY zone 68.5 shekel is your payment ",font=("Helvetica", 12),background= 'gray')
+        self.label1.place(x=170,y=100)
+if __name__ == '__main__':
+    root = Tk()
+    w = 400 # width for the Tk root
+    h = 250 # height for the Tk root
+    
+    # get screen width and height
+    ws = root.winfo_screenwidth() 
+    hs = root.winfo_screenheight() 
+
+    # calculate x and y coordinates for the Tk root window
+    x = (ws/2) - (w/2)
+    y = (hs/2) - (h/2)
+
+    # set the dimensions of the screen and where it is placed
+    root.geometry('%dx%d+%d+%d' % (w, h, x, y))
+    root.title("Number predict")
+    app = App(root)
+    root.mainloop()
